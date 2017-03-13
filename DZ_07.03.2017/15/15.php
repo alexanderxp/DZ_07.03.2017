48c0aa7fecb1add8ca1a3fdd6782752dd7a6d13a
alexanderxp <alexanderxp@ukr.net>
2017-03-13 12:48:49 +0200

    Update 15.php

--- DZ_07.03.2017/15/15.php
+++ DZ_07.03.2017/15/15.php
@@ -1,26 +1,6 @@
 <?php
 
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-<form method=POST action='action.php'>
-    <label for="first">Чило №1<br></label>
-    <input type="text" id="first" name="a"><br>
-    <label for="second">Чило №2<br></label>
-    <input type="text"id="second" name="b"><br>
-    <select name="operator">
-        <option>+</option>
-        <option value="-">-</option>
-        <option value="*">*</option>
-        <option value="/">/</option>
-    </select><br><br>
-    <input type="submit" value="посчитать">
-</form>
-</body>
-
+include '15.html';
 
 $operator = $_POST ['operator'];
 $a = $_POST ['a'];
